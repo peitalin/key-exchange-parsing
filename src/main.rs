f1aa45a8f18447e2850b5fa21c4b852fdbe1d7d4
Peita Lin <n6378056@gmail.com>
2018-06-18 00:18:20 -0700

    parse dates

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,14 @@
+
 #![allow(dead_code)]
 
-extern crate sha2;
-extern crate sha3;
-extern crate hmac;
-extern crate rand;
-extern crate num;
+mod key_exchange;
+mod parse_date;
 
-// use std::io;
-// use std::u64;
-// use sha2::Sha256;
-use num::bigint::{ BigUint };
-use num::pow;
+extern crate chrono;
+extern crate proptest;
 
 
 fn main() {
-    // Must agree on a shared prime and shared base
-    let mut shared_prime: usize = 23;
-    // p
-    let mut shared_base: usize = 5;
-    // g
-    // Alice and Bob then have their own secret keys
-    let mut alice_secret: usize = 6;
-    // a
-    let mut bob_secret: usize = 7;
-    // b
-
-    // Begin
-    println!("Publicly shared variables");
-    println!("\tPublicly shared prime: {}", shared_prime);
-    println!("\tPublicly shared base: {}", shared_base);
-
-    // Alice sends Bob A = g^a mod p
-    let A: usize = (pow(shared_base, alice_secret)) % shared_prime;
-    println!("Alice sends message over public channel: {} (A = g^a mod p)", A);
-
-    // Bob sends to Alice B = g^b mod p
-    let B: usize = (pow(shared_base, bob_secret)) % shared_prime;
-    println!("Bob sends message over public channel: {} (B = g^b mod p)\n", B);
-
-    // Alice calculates secret
-    let alice_shared_secret: usize = (pow(B, alice_secret)) % shared_prime;
-    println!("Alice privately calculates secret:\n\ts = A^b mod p = {}", alice_shared_secret);
-
-    // Bob calculates secret
-    let bob_shared_secret: usize = (pow(A, bob_secret)) % shared_prime;
-    println!("Bob privately calculates secret:\n\ts = B^a mod p = {}", bob_shared_secret);
-
-    println!("Alice and Bob can use the shared secret key to communicate over insecure channels")
+    key_exchange::key_exchange();
+    // parse_date::parse_date("17th June 2018");
 }
---
+++ src/parse_date.rs
@@ -0,0 +1,107 @@
+
+#[cfg(test)]
+#[cfg(macro_use)]
+
+use chrono;
+use chrono::NaiveDate;
+use proptest;
+use std;
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    ParseError,
+    InvalidDateError,
+    InvalidDayError,
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(t: std::num::ParseIntError) -> Error {
+        Error::ParseError
+    }
+}
+
+
+pub fn parse_month(month: &str) -> Option<usize> {
+    let months = vec![
+        "January"
+        , "February"
+        , "March"
+        , "April"
+        , "May"
+        , "June"
+        , "July"
+        , "August"
+        , "September"
+        , "October"
+        , "November"
+        , "December"
+    ];
+    match months.iter().position(|&elem| elem == month) {
+        Some(index) => Some(index + 1),
+        None => None,
+    }
+}
+
+pub fn parse_day(day_with_ordinal: &str) -> Result<u32, Error> {
+    let day: u32 = day_with_ordinal
+        .chars()
+        .take_while(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse::<u32>()?;
+
+    let ordinal = day_with_ordinal
+        .chars()
+        .skip_while(|c| c.is_digit(10))
+        .collect::<String>();
+
+    match (day, ordinal.as_ref()) {
+        (1, "st")
+        | (2, "nd")
+        | (3, "rd")
+        | (4...20, "th")
+        | (21, "st")
+        | (22, "nd")
+        | (23, "rd")
+        | (24...30, "th")
+        | (31, "st") => Ok(day),
+        _ => Err(Error::InvalidDayError)
+    }
+}
+
+pub fn parse_date(humandate: impl AsRef<str>) -> Result<NaiveDate, Error> {
+    let parts: Vec<&str> = humandate.as_ref().split_whitespace().collect();
+    if parts.len() != 4 {
+        return Err(Error::ParseError);
+    }
+    let day: u32 = parse_day(parts[0])?;
+    // TO DO: parts[1] == of;
+    let month: u32 = parse_month(parts[2]).ok_or_else(|| Error::ParseError)? as u32;
+    let year: i32 = parts[3].parse()?;
+    NaiveDate::from_ymd_opt(year, month, day).ok_or_else(|| Error::InvalidDateError)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            parse_date("4th of September 2015"),
+            Ok(NaiveDate::from_ymd(2015, 9, 4))
+        );
+        assert_eq!(
+            parse_date("21st of December 2015"),
+            Ok(NaiveDate::from_ymd(2015, 12, 21))
+        );
+    }
+
+    // proptest! {
+    //     #[test]
+    //     fn doesnt_crash(ref s in "\\PC*") {
+    //         parse(s);
+    //     }
+    // }
+
+}
