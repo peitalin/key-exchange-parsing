b4f571eb4d816be3be5f0383db7bfdf59698b460
Peita Lin <n6378056@gmail.com>
2018-06-21 13:33:44 -0700

    parse-date cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,8 @@ use _3_enums_traits::Language;
 fn main() {
     // _1_key_exchange
     _1_key_exchange::key_exchange();
-
     // _2_parse_date
     _2_parse_date::print_parsed_date("17th of June 2018");
-    // match _2_parse_date::parse_date("17th of June 2018") {
-    //     Ok(d) => println!(
-    //             "\nThe date input is: {}\nWhich is: {}",
-    //             d.format("%A, %-d %B, %C%y").to_string(),
-    //             d.format("%Y-%m-%d").to_string()
-    //         ),
-    //     Err(_e) => println!("\nInvalid date!"),
-    // }
-    //
     let greeter = _3_enums_traits::Greeter::new().with_language(Language::German);
 
     _4_listc::listc();
